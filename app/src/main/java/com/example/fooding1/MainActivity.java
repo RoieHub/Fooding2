ef31ac63497d21c56081d77743dd19f6b87fc8fe
RoieHub <roiemalykin@gmail.com>
2020-11-27 10:46:32 +0200

    added buttons to main activity

--- app/src/main/java/com/example/fooding1/MainActivity.java
+++ app/src/main/java/com/example/fooding1/MainActivity.java
@@ -1,7 +1,8 @@
 package com.example.fooding1;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
