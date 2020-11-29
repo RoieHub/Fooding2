f324fa42c6644c3e77eb6dc26b3aa384cd85b3a4
RoieHub <roiemalykin@gmail.com>
2020-11-29 14:28:22 +0200

    added notes to MainActivity.java and new activity_user_search_page.xml

---
+++ app/src/main/java/com/example/fooding1/user_search_page.java
@@ -0,0 +1,14 @@
+package com.example.fooding1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class user_search_page extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_search_page);
+    }
+}
